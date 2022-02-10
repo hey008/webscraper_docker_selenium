27aee4112e81bd4aca7b1b3de42ae57bb06f41b2
Jane Maverick <hey008@gmail.com>
2022-02-10 09:58:53 +0700

    - Improve performance - Correct name of Log File

--- py_scripts/myselenium.py
+++ py_scripts/myselenium.py
@@ -1,3 +1,4 @@
+from ast import Global
 import requests
 from time import sleep
 from datetime import datetime
@@ -10,44 +11,55 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
 
-def web_scraper(url, drivertype='remote'):
+# Define Variables
+browser_mode = 'Remote' # Remote: Docker Browser, Local: Machine Browser
+debug_mode = 0 # 0: Off, 1: On 
+
+def web_scraper(url, drivertype='Remote'):
+    global browser_mode
+    browser_mode = drivertype
+    
     if url == "":
         print(f'URL is empty')
         return False
 
     print(f'web_scraper: {url}')
     
-    #load_time = get_load_time(url)
-    #print(f'Check : Page load time: {load_time} seconds')
-
-    browser = get_browser(drivertype)
-    connect_browser(url, browser)
-    sleep(10)
-    html = browser.page_source
-    write_2_file(html)
+    if url_status(url):
+        browser = get_browser(drivertype)
+        connect_browser(url, browser)
+        sleep(10)
+        html = browser.page_source
+        write_2_file(html)
+        browser.quit()
+        return True
+    
     return False
 
 # Functional library ----- ----- ----- ----- ----- ----- ----- ----- ----- 
 
-def get_browser(drivertype='remote'):
+def get_browser(drivertype='Remote'):
     # initialize options
     options = webdriver.ChromeOptions()
     # pass in headless argument to options
     options.add_argument('--headless')
     options.add_argument("--no-sandbox")
-    options.add_argument("--screen-size=1200x800")
+    options.add_argument("--screen-size=1280x800")
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
     # initialize driver
-    if drivertype == 'remote':
+    if drivertype == 'Remote':
         options.add_argument("--disable-gpu")
         #options.add_argument("--remote-debugin-port=9222")
         driver = webdriver.Remote(
                 command_executor='http://localhost:4444/wd/hub',
-                desired_capabilities=DesiredCapabilities.CHROME)
+                desired_capabilities=DesiredCapabilities.CHROME,
+                options=options)
     else:
-        driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     return driver
 
-def connect_browser(url, browser):    
+def connect_browser(url, browser):
+    global debug_mode
     try:
         browser.get(url)
         WebDriverWait(browser, 5).until(
@@ -56,24 +68,33 @@ def connect_browser(url, browser):
         return True
     except Exception as ex:
         print(f'Error connecting to {url}.')
+        if debug_mode == 1:
+            print(f'{ex}')
     return False
 
-def get_load_time(article_url):
+def url_status(url):
+    result = False
     try:
         # set headers
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 OPR/83.0.4254.27'}
         # make get request to article_url
         response = requests.get(
-            article_url, headers=headers, stream=True, timeout=3.000)
+            url, headers=headers, stream=True, timeout=3.000)
         # get page load time
         load_time = response.elapsed.total_seconds()
+        print(f'URL Check : Page load time: {load_time} seconds')
+        result = True
     except Exception as ex:
-        load_time = 'Loading Error'
-    return load_time
+        print(f'URL Check : Page load time: Error')
+        if debug_mode == 1:
+            print(f'{ex}')
+    return result
 
 def write_2_file(html):
+    global browser_mode
+
     current_date = str(datetime.now())
-    filename = "HTMLs/"+str(current_date).replace(":","").replace(" ","_").replace("-","") + ".html"
+    filename = "HTMLs/"+ browser_mode +"-"+ str(current_date).replace(":","").replace(" ","_").replace("-","") + ".html"
     soupobj = BeautifulSoup(html, 'html.parser')
 
     f = open(filename, "a")
--- services.py
+++ services.py
@@ -1,24 +1,28 @@
 import sys
 from py_scripts.myselenium import web_scraper
 
-# ---
-# print (f'__name__: {__name__}')
-# ---
-
+browser_mode = ''; # Local, Remote
 
 # ---
-print (f'1: Sample Test: Remote')
-print (f'2: Sample Test: Local')
+print (f'1: Sample Test: Local')
+print (f'2: Sample Test: Remote')
+print (f'3: Custom URL: Local')
+print (f'4: Custom URL: Remote')
 menu = input('Please select: ')
 
 if (menu == '1'):
-    web_scraper('https://www.welovephuket.com')
+    browser_mode = 'Local'
+    web_scraper('https://www.welovephuket.com', browser_mode)
 elif (menu == '2'):
-    web_scraper('https://www.welovephuket.com', 'Local')
+    browser_mode = 'Remote'
+    web_scraper('https://www.welovephuket.com', browser_mode)
 elif (menu == '3'):
-    url = input('Enter URL: ')
-    web_scraper(url)
+    browser_mode = 'Local'
+    url = input('Enter URL (Eg. https://jane-maverick.com): ')
+    web_scraper(url, browser_mode)
 elif (menu == '4'):
-    print(f'Call CSV')
+    browser_mode = 'Remote'
+    url = input('Enter URL (Eg. https://jane-maverick.com): ')
+    web_scraper(url, browser_mode)
 else:
     print(f'Command not found')
