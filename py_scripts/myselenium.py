846701f3b0a05d98d0d60c88479bffc90411885a
Jane Maverick <hey008@gmail.com>
2022-02-11 15:08:02 +0700

    Making Json file object

--- py_scripts/myselenium.py
+++ py_scripts/myselenium.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import json
 from time import sleep
 from datetime import datetime
 from bs4 import BeautifulSoup
@@ -14,7 +15,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 # Define Variables
 storage_path = 'Storage/'
 browser_mode = 'Remote' # Remote: Docker Browser, Local: Machine Browser
-debug_mode = 0 # 0: Off, 1: On 
+debug_mode = 0 # 0: Off, 1: On
+json_data = {
+    'URL': "",
+    'Revisions': "",
+    'Date': "",
+    'file_path': {},
+    'head_tags': {},
+    'html_tags': {}
+}
 
 def web_scraper(url, drivertype='Remote'):
     global browser_mode
@@ -24,12 +33,33 @@ def web_scraper(url, drivertype='Remote'):
         print(f'URL is empty')
         return False
 
-    os.makedirs(storage_path + 'HTMLs', exist_ok=False)
-    os.makedirs(storage_path + 'Screenshot', exist_ok=False)
+    try:
+        os.makedirs(storage_path + 'HTMLs', exist_ok=False)
+        print(f'{str(datetime.now())} | HTMLs directory created')
+    except OSError as error:
+        print(f'{str(datetime.now())} | HTMLs directory exists')
+    try:
+        os.makedirs(storage_path + 'Screenshot', exist_ok=False)
+        print(f'{str(datetime.now())} | Screenshot directory created')
+    except OSError as error:
+        print(f'{str(datetime.now())} | Screenshot directory exists')
+    try:
+        os.makedirs(storage_path + 'Json', exist_ok=False)
+        print(f'{str(datetime.now())} | Json directory created')
+    except OSError as error:
+        print(f'{str(datetime.now())} | Json directory exists')
     
     file_name = browser_mode +"-"+ str(datetime.now()).replace(":","").replace(" ","_").replace("-","")
     file_html = storage_path + 'HTMLs/'+ file_name +'.html'
     file_screenshot = storage_path + 'Screenshot/'+ file_name + '.png'
+    file_json = storage_path + 'Json/'+ file_name + '.json'
+
+    json_data['URL'] = url
+    json_data['Revisions'] = file_name
+    json_data['Date'] = str(datetime.now())
+    json_data['file_path']['HTML'] = file_html
+    json_data['file_path']['Screenshot'] = file_screenshot
+    json_data['file_path']['Json'] = file_json
 
     print(f'{str(datetime.now())} | Scrape URL: {url}')
     
@@ -47,10 +77,9 @@ def web_scraper(url, drivertype='Remote'):
             if debug_mode == 1:
                 print(f'{ex}')
         browser.quit()
-        write_json_file(html)
+        write_json_file(html, file_json)
         print(f'{str(datetime.now())} | Scrape URL Completed')
         return True
-    
     return False
 
 # Functional library ----- ----- ----- ----- ----- ----- ----- ----- ----- 
@@ -114,11 +143,49 @@ def write_html_file(html, filename=""):
         filename = "HTMLs/"+ str(current_date).replace(":","").replace(" ","_").replace("-","") + ".html"
     
     soupobj = BeautifulSoup(html, 'html.parser')
+    html = str(soupobj)
 
-    f = open(filename, "a")
-    f.write(str(soupobj.encode("utf-8")))
+    f = open(filename, "a", encoding='utf-8')
+    f.write(html)
     f.close()
     print(f'{str(datetime.now())} | HTML File Created: {filename}')
 
-def write_json_file(html, finename=""):
+def chk_tags(tags):
+    r = []
+    for tag in tags:
+        if tag.text.strip() != "":
+            r.append(tag.text.strip())
+    return r
+
+def write_json_file(html, filename=""):
+    if filename == "":
+        current_date = str(datetime.now())
+        filename = storage_path +"Json/"+ str(current_date).replace(":","").replace(" ","_").replace("-","") + ".json"
+    
+    soupobj = BeautifulSoup(html, 'html.parser')
+
+    head_tags_title = soupobj.title.text
+    if head_tags_title:
+        json_data['head_tags']['title'] = head_tags_title
+
+    head_tags_description = soupobj.find('meta',{'name':'description'})
+    if head_tags_description:
+        json_data['head_tags']['description'] = head_tags_description.get('content')
+    
+    head_tags_keywords = soupobj.find('meta',{'name':'keywords'})
+    if head_tags_keywords:
+        json_data['head_tags']['keywords'] = list(filter(None, [s.strip() for s in head_tags_keywords.get('content').split(",")]))
+    
+    tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'footer', 'address', 'div', 'li', 'td', 'label', 'b', 'strong', 'span', 'center']
+    for tag in tags:
+        cl = chk_tags(soupobj.find_all(tag))
+        if len(cl) > 0:
+            json_data['html_tags'][tag] = cl
+
+    json_load = json.dumps(json_data)
+    # print(json_load) # <-- Debug
+    f = open(filename, 'w')
+    f.write(json_load)
+    f.closed
+    print(f'{str(datetime.now())} | Json File Created: {filename}')
     return True
