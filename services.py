025ed97113e951936d2113ac22c6238f1a4a1184
Jane Maverick <hey008@gmail.com>
2022-02-14 13:09:22 +0700

    Add read sample file

--- services.py
+++ services.py
@@ -1,4 +1,5 @@
-import sys
+from os import listdir
+from os.path import isfile, join
 from py_scripts.myselenium import web_scraper
 
 browser_mode = ''; # Local, Remote
@@ -8,6 +9,7 @@ print (f'1: Sample Test: Local')
 print (f'2: Sample Test: Remote')
 print (f'3: Custom URL: Local')
 print (f'4: Custom URL: Remote')
+print (f'5: Sample File')
 menu = input('Please select: ')
 
 if (menu == '1'):
@@ -24,5 +26,27 @@ elif (menu == '4'):
     browser_mode = 'Remote'
     url = input('Enter URL (Eg. https://jane-maverick.com): ')
     web_scraper(url, browser_mode)
+elif (menu == '5'):
+    browser_mode = 'Local'
+    file_path = 'sample'
+    files = [f for f in listdir(file_path) if isfile(join(file_path, f))]
+    # print(f'{files}')
+    for file in files:
+        print(f'{file}')
+        ofile = open(file_path +'/'+ file, 'r')
+        count = 0
+        while True:
+            file_line = ofile.readline().strip()
+            if not file_line or count == 10:
+                break
+            count += 1
+            print(f'{count} | {file_line}')
+            if file_line.startswith('http://') or file_line.startswith('https://'):
+               web_scraper(file_line, browser_mode)
+            elif web_scraper('https://'+ file_line, browser_mode):
+                continue
+            elif web_scraper('http://'+ file_line, browser_mode):
+                continue
+        ofile.close()
 else:
     print(f'Command not found')
